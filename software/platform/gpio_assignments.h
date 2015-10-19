a0d49bf6538d7ab33be2ecaefd36e38145d86f7d
Dinker Ambe <dinkerambe@gmail.com>
2015-10-19 18:17:15 -0400

    GPIO pin assignments file created

---
+++ software/platform/gpio_assignments.h
@@ -0,0 +1,12 @@
+#ifndef GPIO_ASSIGNMENTS_H_
+#define GPIO_ASSIGNMENTS_H_
+
+// GPIO
+#define GPIO_HALL_EFFECT_1 0
+#define GPIO_HALL_EFFECT_2 0
+
+#define GPIO_TURN_BUTTON_1 0
+#define GPIO_TURN_BUTTON_2 0
+
+
+#endif // GPIO_ASSIGNMENTS_H_
