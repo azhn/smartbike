df20e14056a1804a5d2a4fa6ddf0fa139e3a44fd
Dinker Ambe <dinkerambe@gmail.com>
2015-11-11 00:51:25 -0500

    Created a driver to manage producing data from accelerometer with averaging

---
+++ software/drivers/AccelDataDriver.c
@@ -0,0 +1,41 @@
+#include <stdbool.h>
+#include "AccelerometerControl.h"
+#include "AccelDataDriver.h"
+
+
+int16_t readData(AccelDataType type) {
+    if (type == DATA_X) {
+        return readAxisX();
+    } else if (type == DATA_Y) {
+        return readAxisY();
+    } else if (type == DATA_Z) {
+        return readAxisZ();
+    }
+    return ACCEL_DATA_ERROR;
+}
+void populateAccelDataBank() {
+    int8_t i;
+    for (i=0; i < _NUM_ACCEL_DATA; ++i) {
+        if (_accel_poll[i]) {
+            // Increment counter in the beginning
+            _AccelDataBank._internal_count[i]++;
+            // greater than ensures that we don't throw out our n-sampled value
+            if (_AccelDataBank._interal_count[i] > MAX_DATA_COUNT) {
+                _AccelDataBank._interal_count = 0;
+                _AccelDataBank._accel_data[i] = readData((Acceldata)i);
+            } else {
+                _AccelDataBank._accel_data[i] += readData((Acceldata)i);
+            }
+        }
+    }
+}
+
+void grabAccelData(AccelDataType type, uint16_t* out, uint16_t *ppos) {
+    if (_AccelDataBank._accel_poll[type] &&
+        _AccelDataBank._interal_count[type] == MAX_DATA_COUNT) {
+        *out = _AccelDataBank._accel_data[type] >> DATA_BITSHIFT_DIVISOR;
+        if (ppos != NULL) {
+            ppos++;
+        }
+    }
+}
---
+++ software/drivers/AccelDataDriver.h
@@ -0,0 +1,54 @@
+#ifndef ACCELDATADRIVER_H_
+#define ACCELDATADRIVER_H_
+
+#include <stdbool.h>
+
+#define DATA_BITSHIFT_DIVISOR 2 // x >> 2 = x/4
+#define MAX_DATA_COUNT (1<<DATA_BITSHIFT_DIVISOR)
+
+// 1<<15 arbitrarily large negative number accelerometer can't produce
+#define ACCEL_DATA_ERROR (1<<(sizeof(int16_t)*8-1)) 
+
+typedef enum AccelDataType {
+    DATA_X,
+    DATA_Y,
+    DATA_Z,
+     _NUM_ACCEL_DATA
+} AccelDataType;
+
+typedef struct AccelData {
+    int16_t _accel_data[_NUM_ACCEL_DATA];
+    bool _accel_poll[_NUM_ACCEL_DATA];
+    uint8_t _internal_count[_NUM_ACCEL_DATA];
+} AccelData;
+
+AccelData _AccelDataBank;
+
+int16_t readData(AccelDataType type);
+
+
+
+/******************************************************************************
+* USER FUNCTIONS
+******************************************************************************/
+// Will populate internal data bank. Should be called whenever Accelerometer
+// has available data in the main loop.
+void populateAccelDataBank();
+
+// Will poll accelerometer and save necessary values for this data type
+void setPollAccelData(AccelDataType type) {
+    _AccelDataBank._accel_poll[type] = true;
+}
+
+// Will disable polling of accelerometer for this data type
+void unsetPollAccelData(AccelDataType type) {
+    _AccelDataBank._accel_poll[type] = false;
+}
+
+// Will only populate (out) if data is available. As an indicator for completion
+// one can pass the pointer to a counter (ppos), and it will be incremented only
+// upon a successful data grab.
+void grabAccelData(AccelDataType type, uint16_t* out, uint16_t *ppos);
+
+
+#endif // DATADRIVER_H_
