16aeaa3a3eff8b87205d3da368ad404465ea7995
Dinker Ambe <dinkerambe@gmail.com>
2015-11-17 13:14:59 -0500

    Fixed up LightControl file

--- software/apps/smartbike/LightControl.h
+++ software/apps/smartbike/LightControl.h
@@ -4,26 +4,26 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define LED_LIGHTS_SIZE 10
 /***************************************************************************
                                   Types
 ***************************************************************************/
-typedef enum {OFF, BLINKING, ON} LightState;
+typedef enum {OFF=0, BLINKING=1, ON=2} LightState;
 
-typedef enum {BRAKE=0, FRONT=1, BACK=2, 
-              TURN=3, _LIGHT_TYPE_SIZE=4} LightType;
-
-typedef enum {CENTER=0, LEFT=0, RIGHT=1, _LIGHT_POS_SIZE =2} LightPosition;
+typedef enum {BRAKE=0, LEFT_TURN=2, RIGHT_TURN=3, _LIGHT_TYPE_SIZE=4} LightType;
+typedef uint32_t pwm_address_t;
 
 typedef struct {
     LightState _state;
-    uint32_t _gpio_pin;
+    pwm_address_t _address;
 } Light;
 
 /****************************************************************************
                                  Globals
 *****************************************************************************/
 /* index by light type, then position */
-Light lights[_LIGHT_TYPE_SIZE][_LIGHT_POS_SIZE]; 
+Light rear_lights[_LIGHT_TYPE_SIZE]; 
+Light led_lights[LED_LIGHTS_SIZE];
 
 /*****************************************************************************
                               Initialization
@@ -35,34 +35,12 @@ void initializeLights();
                           Turning Lights On/Off
 *****************************************************************************/
 /* generic function to set any light state */
-void setLightState(LightType type, LightPosition pos, LightState state );
+void setRearLightState(LightType type, LightState state );
+void setLEDLightState(uint8_t pos);
 
 /* generic function to get any light state */
-LightState getLightState( LightType type, LightPosition pos);
-
-/* turn on front light with the specified state */
-void setFrontLight( LightState state );
-
-/* set the a turn light with the specified state */
-void setTurnLight(LightState state, LightPosition pos);
-
-/* set the back light with the specified state */
-void setBackLight(LightState state);
-
-/* set the brake light with the specified state */
-void setBrakeLight(LightState state);
-
-/*****************************************************************************
-                           Check Light Status
-*****************************************************************************/
-/* check the front light status */
-LightState getfrontLight( );
-
-/* check the turn light status */
-LightState getTurnLight(LightPosition pos);
-
-/* check the back light status */
-LightState backLightStatus( );
+LightState getRearLightState( LightType type );
+LightState getLEDLightState( uint8_t pos );
 
 /*****************************************************************************
                              Global Control
