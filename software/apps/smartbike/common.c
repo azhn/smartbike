6eda47cb9963f97c2fa36066c71800f15a8be06a
Dinker Ambe <dinkerambe@gmail.com>
2015-11-21 16:07:49 -0500

    moved a few files. deleted common.h/.c

--- software/apps/smartbike/common.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-#include <stdlib.h>
-
-DataPair* DataPair_new(uint32_t count, uint32_t time) {
-   DataPair* d = (DataPair*)malloc(sizeof(DataPair));
-   d->count = count;
-   d->time = time;
-   return d;
-}
