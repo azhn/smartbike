2871682aeb5ca8b47c1cd072817691630d2f3c92
Dinker Ambe <dinkerambe@gmail.com>
2015-11-24 19:13:24 -0500

    made states for turn signals

--- software/drivers/LightAction.h
+++ software/drivers/LightAction.h
@@ -34,19 +34,18 @@ enum LedType {
     _NUM_LEDS
 };
 
-//  LIGHT_STATE_OFF=0,
-//  LIGHT_STATE_ON,
-//  LIGHT_STATE_DIM_ON,
-//  LIGHT_STATE_BLINKING,
-//  LIGHT_STATE_BLINKING_OFF,
-//  LIGHT_STATE_BLINKING_ON,
-//  _NUM_LIGHT_STATE
-
-/*static LightState[_NUM_LIGHT_ACTION][_NUM_LIGHT_TYPE] = {
+const static LightState _light_action_to_states[_NUM_LIGHT_ACTION][_NUM_LIGHT_TYPE] = {
 {
     // CENTER                   LEFT                    RIGHT
-    {LIGHT_STATE_ON_DIM,        LIGHT_STATE_OFF,        LIGHT_STATE_OFF}, // LIGHT_ACTION_NONE 
-}*/
+    {LIGHT_STATE_ON_DIM,        LIGHT_STATE_OFF,        LIGHT_STATE_OFF},      // LIGHT_ACTION_NONE 
+    {LIGHT_STATE_ON,            LIGHT_STATE_ON,         LIGHT_STATE_ON},       // LIGHT_ACTION_BRAKE
+    {LIGHT_STATE_ON_DIM,        LIGHT_STATE_BLINKING,   LIGHT_STATE_OFF},      // LIGHT_ACTION_LEFT_TURN,
+    {LIGHT_STATE_ON,            LIGHT_STATE_BLINKING,   LIGHT_STATE_ON},       // LIGHT_ACTION_LEFT_TURN_BRAKE,
+    {LIGHT_STATE_ON_DIM,        LIGHT_STATE_OFF,        LIGHT_STATE_BLINKING}, // LIGHT_ACTION_RIGHT_TURN,
+    {LIGHT_STATE_ON,            LIGHT_STATE_OFF,        LIGHT_STATE_BLINKING}  // LIGHT_ACTION_RIGHT_TURN_BRAKE,
+
+};
+
 void performLightAction(const State* state, const LightAction* light_action, uint8_t count);
 // Takes the state of the rear turn lights, and correctly drives the led indicators
 static void set_led_turn(const LightState* left_state, const LightState* right_state);
