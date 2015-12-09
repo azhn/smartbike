d21313225c643e31087993fa159aff43feaadcfd
Charles Hardin <chhardin@umich.edu>
2015-12-09 16:49:58 -0500

    changed servo arrays to appropriate numbers, added a check to make sure we cant attempt to shift to a gear that indexes out of the array.

--- software/drivers/ServoControl.c
+++ software/drivers/ServoControl.c
@@ -1,51 +1,22 @@
 #include "BikeTimers.h"
 #include "ServoControl.h"
 
-static const uint16_t REAR_GEAR_UP[7] = {325, 345, 352, 352, 365, 372, 372};
-static const uint16_t REAR_GEAR_DOWN[7] = {305, 318, 330, 330, 345, 372, 372};
-static const uint16_t FRONT_GEAR_UP[7] = {430, 430, 430, 344, 344, 349, 349};
-static const uint16_t FRONT_GEAR_DOWN[7] = {425, 425, 425, 344, 344, 349, 349};
+static const uint16_t REAR_GEAR_UP[6] = {335, 358, 364, 387, 400, 400};
+static const uint16_t REAR_GEAR_DOWN[6] = {322, 340, 357, 376, 400, 400};
+static const uint16_t FRONT_GEAR_UP[6] = {305, 305, 305, 285, 285, 225};
+static const uint16_t FRONT_GEAR_DOWN[6] = {305, 305, 305, 305, 325, 230};
 
-
-/*static const uint16_t REAR_GEAR_UP[7] =           {325, 345, 352, 352, 365, 372, 372};
-static const uint16_t REAR_GEAR_UP_OVERSHOOT[7] = {325, 345, 352, 352, 365, 372, 372};
-
-static const uint16_t REAR_GEAR_DOWN[7] =           {308, 318, 330, 330, 345, 372, 372};
-static const uint16_t REAR_GEAR_DOWN_OVERSHOOT[7] = {303, 318, 330, 330, 345, 372, 372};
-
-static const uint16_t FRONT_GEAR_UP[7] =           {430, 430, 430, 366, 366, 366, 366};
-static const uint16_t FRONT_GEAR_UP_OVERSHOOT[7] = {430, 430, 430, 310, 310, 310, 310};
-
-static const uint16_t FRONT_GEAR_DOWN[7] =           {425, 425, 425, 366, 366, 366, 366};
-static const uint16_t FRONT_GEAR_DOWN_OVERSHOOT[7] = {425, 425, 425, 366, 366, 366, 366};
-*/
-// REAR_UP:        325 345 352
-// REAR_UP_OVER:   325 345 352
-// REAR_DOWN:      308 318 330
-// REAR_DOWN_OVER: 300 318 330
-
-// FRONT_UP:       430 430 430
-// FRONT_UP_OVER:  430 430 430
-// FRONT_DOWN:     425 425 425
-// FRONT_DOWN_OVER:425 425 425
-// 
-/*static const uint16_t REAR_GEAR_UP[7] =           {325, 345, 352,352,352,352,352};
-static const uint16_t REAR_GEAR_UP_OVERSHOOT[7] = {325, 345, 330,352,352,352,352};
-
-static const uint16_t REAR_GEAR_DOWN[7] =           {308, 318, 330,330,330,330,330};
-static const uint16_t REAR_GEAR_DOWN_OVERSHOOT[7] = {300, 318, 330,330,330,330,330};
-
-static const uint16_t FRONT_GEAR_UP[7] =           {425, 425, 425, 366,366,366,366};
-static const uint16_t FRONT_GEAR_UP_OVERSHOOT[7] = {425, 425, 425, 302,302,302,302};
-
-static const uint16_t FRONT_GEAR_DOWN[7] =           {425, 425, 425, 366,366,366,366};
-static const uint16_t FRONT_GEAR_DOWN_OVERSHOOT[7] = {425, 425, 425, 366,366,366,366};*/
 
 void update_servos(State* state)
 {
     static uint32_t timer = 0;
 
     static bool over_shoot = false;
+
+	if(state->target_gear >= NUM_GEARS)
+	{
+		state->target_gear = NUM_GEARS - 1;
+	}
 
     if(state->target_gear == state->curr_gear)
 //    if(state->target_gear == state->curr_gear && !over_shoot)
