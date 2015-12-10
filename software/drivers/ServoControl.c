4385d449fd60dd4438b25e04788da106695bd6e5
Charles Hardin <chhardin@umich.edu>
2015-12-09 22:00:35 -0500

    make update gear check for manual or auto mode

--- software/drivers/ServoControl.c
+++ software/drivers/ServoControl.c
@@ -19,11 +19,11 @@ void update_servos(State* state)
 	}
 
     if(state->target_gear == state->curr_gear)
-//    if(state->target_gear == state->curr_gear && !over_shoot)
     {
         //	return;
-    } else {
-
+    } 
+	else 
+	{
         if(state->target_gear > state->curr_gear)
         {
             state->shift_dir = UP; 
@@ -35,34 +35,16 @@ void update_servos(State* state)
 
         if(state->shift_dir == UP) 
         {
- /*           if (!over_shoot) {
-                pca9685_setPWM(REAR_PWM, 0, REAR_GEAR_UP_OVERSHOOT[state->target_gear]);
-                pca9685_setPWM(FRONT_PWM, 0, FRONT_GEAR_UP_OVERSHOOT[state->target_gear]);
-
-                over_shoot = true;
-                timer = get_millis();
-            } else if (get_millis() - timer > 500) {*/
                 pca9685_setPWM(REAR_PWM, 0, REAR_GEAR_UP[state->target_gear], PWM0_ADDR);
                 pca9685_setPWM(FRONT_PWM, 0, FRONT_GEAR_UP[state->target_gear], PWM0_ADDR);
-/*                over_shoot = false;
-            }*/
         }
         else
         {
-/*            if (!over_shoot) {
-                pca9685_setPWM(REAR_PWM, 0, REAR_GEAR_DOWN_OVERSHOOT[state->target_gear]);
-                pca9685_setPWM(FRONT_PWM, 0, FRONT_GEAR_DOWN_OVERSHOOT[state->target_gear]);
-
-                over_shoot = true;
-                timer = get_millis();
-            } else if (get_millis() - timer > 500) {*/
                 pca9685_setPWM(REAR_PWM, 0, REAR_GEAR_DOWN[state->target_gear], PWM0_ADDR);
                 pca9685_setPWM(FRONT_PWM, 0, FRONT_GEAR_DOWN[state->target_gear], PWM0_ADDR);
-/*                over_shoot = false;
-            }*/
         }
 
         state->curr_gear = state->target_gear;
-        state->flags[PEDAL_FLAG] = false;
     }
+        state->flags[PEDAL_FLAG] = false;
 }
